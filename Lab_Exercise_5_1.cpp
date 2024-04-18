972ca2ec165086a2e108ad8b23dbc186e358828e
“RashidSidreck” <Bsc-com-ne-10-22@unima.ac.mw>
2024-04-18 22:01:54 +0200

    part one of exercise

---
+++ Lab_Exercise_5_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    srand(time(nullptr));
+    
+    int number_of_days= rand() % 12;
+
+    if(number_of_days<=10){
+        cout<<"Your subscription will expire soon. Renew now!"<<endl;
+    }
+    else if(number_of_days<=5){
+        cout<<"Your subscription expires in "<<number_of_days<<" Renew now and save 10%!"<<endl;
+    }
+    else if(number_of_days==1){
+        cout<<"Your subscription expires within a day!\n""Renew now and save 20%!"<<endl;
+    }
+    else if(number_of_days==0){
+        cout<<"Your subscription has expired."<<endl;
+    }
+    else{
+         cout<<"You have an active subscription."<<endl;}
+
+         return 0;
+       
+}
