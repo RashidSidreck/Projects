264caa3c20a1c0b06b5eebf06e1bdbf7a5abb256
“RashidSidreck” <Bsc-com-ne-10-22@unima.ac.mw>
2024-04-19 17:17:40 +0200

    Part three of Exercise one

--- Lab_Exerxise_5_3.cpp
+++ Lab_Exerxise_5_3.cpp
@@ -4,4 +4,10 @@ using namespace std;
 
 int main(){
     string array[]={"B123","C234","A345","C15","B17","G3003","C235","B179"};
+
+    for(int i=0; i<=sizeof(array); ++i){
+        if(array[i][0] =='B'){
+            cout<<array[i]<<endl;
+        }
+    }
 }
